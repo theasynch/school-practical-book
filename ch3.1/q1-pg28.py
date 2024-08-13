440927e71bccb0b42608263f2cf61ab77620df8a
theasynch <omawthankar16032007@gmail.com>
2024-08-13 21:20:49 +0530

    Create q1-pg28.py
    Co-Authored-By: Krish Parikh <152317895+KrishParikh01@users.noreply.github.com>

---
+++ ch3.1/q1-pg28.py
@@ -0,0 +1,40 @@
+"""
+Part A
+
+Function name : sub1
+Arguments: a,b
+Number of statements in function body: 1
+Returned Values: None
+Function Work: To find the difference between a and b and printing it.
+"""
+
+"""
+Part B
+
+Function name : cube2
+Arguments: x
+Number of statements in function body: 1
+Returned Values: x*x*x-x
+Function Work: To find the cube of x and returning it.
+"""
+
+"""
+Part C
+
+Function name : cube1
+Arguments: x
+Number of statements in function body: 0
+Returned Values: x*x*x
+Function Work: simply returning the cube of x.
+"""
+
+"""
+Part D
+
+Function name : greeting
+Arguments: None
+Number of statements in function body: 1
+Returned Values: None
+Function Work: Simply printing "Welcome to Python".
+"""
+
